a96d5b16e83c656c2f1c740b5d123a585cb9bf40
Mohammed.Saeb <Mohammed.saeb98@hotmail.com>
2020-01-31 20:39:15 +0300

    prettier applied

--- Natakallam/frontity.settings.js
+++ Natakallam/frontity.settings.js
@@ -1,57 +1,42 @@
 const settings = {
-  "name": "Natakallam",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.io",
-      "title": "Nataklam",
-      "description": "Learn a language. Change a life."
-    }
+  name: 'Natakallam',
+  state: {
+    frontity: {
+      url: 'https://test.frontity.io',
+      title: 'Nataklam',
+      description: 'Learn a language. Change a life.',
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Learn Language",
-              "/conversation-sessions/"
-            ],
-            [
-              "Academic Programs",
-              ""
-            ],
-            [
-              "Refugee Voices",
-              "/refugee-voices/"
-            ],
-            [
-              "About Us",
-              "/about/"
-            ]
+      name: '@frontity/mars-theme',
+      state: {
+        theme: {
+          menu: [
+            ['Home', '/'],
+            ['Learn Language', '/conversation-sessions/'],
+            ['Academic Programs', ''],
+            ['Refugee Voices', '/refugee-voices/'],
+            ['About Us', '/about/'],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "https://natakallam.com/wp-json"
-        }
-      }
+      name: '@frontity/wp-source',
+      state: {
+        source: {
+          api: 'https://natakallam.com/wp-json',
+        },
+      },
     },
-    "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    '@frontity/tiny-router',
+    '@frontity/html2react',
+  ],
 };
 
 export default settings;
